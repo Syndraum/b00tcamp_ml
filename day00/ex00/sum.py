0804d830ff47f83fbc03005a8d4f78e8e7e09a73
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2020-01-20 15:09:54 +0100

    mean

--- day00/ex00/sum.py
+++ day00/ex00/sum.py
@@ -4,14 +4,20 @@ import numpy as np
 def sum_(x, f):
     mysum = 0.0
     try:
-        if x.size == 0:
+        if not isinstance(x, np.ndarray) or x.size == 0:
             return None
         for value in x:
-            mysum += f(value)
+            mysum += float(f(value))
         return mysum
-    except (TypeError, AttributeError):
+    except (TypeError) as e:
         return None
 
 
-X = np.array([])
-print(sum_(3, lambda x: x+2))
+def mean(x):
+    return sum_(x, lambda x: x) / len(x)
+
+
+X1 = np.array([])
+X = np.array([0, 15, -9, 7, 12, 3, -21])
+# print(sum_(X, lambda x: x**2))
+print(mean(X**2))
---
+++ day00/ex01/mean.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def mean(x):
+    
