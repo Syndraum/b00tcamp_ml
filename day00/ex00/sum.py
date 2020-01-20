1626e1b8c0c92b72cb3d01b894881d8cb1896750
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2020-01-20 17:05:23 +0100

    mat_vec_prod

--- day00/ex00/sum.py
+++ day00/ex00/sum.py
@@ -52,11 +52,32 @@ def dot(x, y):
         return mysum
 
 
+def mat_vec_prod(x, y):
+    if not isinstance(x, np.ndarray) or x.size == 0:
+        return None
+    if not isinstance(y, np.ndarray) or y.size == 0:
+        return None
+    if len(x.shape) != 2 or x.shape[1] != len(y):
+        return None
+    lst = np.array([])
+    for vector in x:
+        lst = np.append(lst, dot(vector, y))
+    return np.expand_dims(lst, axis=1).astype(int)
+
+
 Z = np.array([0])
-X = np.array([0, 15, -9, 7, 12, 3, -21])
-Y = np.array([2, 14, -13, 5, 12, 4, -19])
+X = np.array([0, 15, -9, 7, 12, 3, -21]).reshape((7, 1))
+Y = np.array([2, 14, -13, 5, 12, 4, -19]).reshape((7, 1))
+W = np.array([
+    [-8, 8, -6, 14, 14, -9, -4],
+    [2, -11, -2, -11, 14, -2, 14],
+    [-13, -2, -5, 3, -8, -4, 13],
+    [2, 13, -14, -15, -14, -15, 13],
+    [2, -1, 12, 3, -7, -3, -6]])
 # print(sum_(X, lambda x: x**2))
 # print(mean(X**2))
 # print(variance(X))
 # print(std(Y))
-print(dot(Z, Y))
+# print(dot(Z, Y))
+print(mat_vec_prod(W, Y))
+# print(W.dot(X))
