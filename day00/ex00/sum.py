5ed4c61cb526136021a84402cfc224c23b5d723b
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2020-01-20 22:12:44 +0100

    ex10

--- day00/ex00/sum.py
+++ day00/ex00/sum.py
@@ -102,6 +102,18 @@ def vec_mse(y, y_hat):
     return dot(tmp, tmp)/len(y)
 
 
+def linear_mse(x, y, theta):
+    mysum = 0.0
+    for i in range(len(y)):
+        mysum += (dot(theta, x[i]) - y[i])**2
+    return mysum/len(y)
+
+
+def vec_linear_mse(x, y, theta):
+    tmp = (x.dot(theta) - y)
+    return dot(tmp, tmp)/len(y)
+
+
 Z = np.array([0])
 X = np.array([0, 15, -9, 7, 12, 3, -21])
 X1 = np.array([0, 15, -9, 7, 12, 3, -21])
@@ -129,5 +141,20 @@ Z = np.array([
 # print(defmat_mat_prod(W, Z))
 # print(defmat_mat_prod(Z, W))
 # print(W.dot(X))
-print(vec_mse(X, Y))
-print(vec_mse(X, X))
+# print(vec_mse(X, Y))
+# print(vec_mse(X, X))
+
+X = np.array([
+    [ -6, -7, -9],
+    [ 13, -2, 14],
+    [ -7, 14, -1],
+    [ -8, -4, 6],
+    [ -5, -9, 6],
+    [ 1, -5, 11],
+    [ 9, -11, 8]])
+Y = np.array([2, 14, -13, 5, 12, 4, -19])
+Z = np.array([3,0.5,-6])
+W = np.array([0,0,0])
+
+print(vec_linear_mse(X, Y, Z))
+print(vec_linear_mse(X, Y, W))
