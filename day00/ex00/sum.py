75f37f1ac92f203c25f16941bec4b8df1ef78c4d
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2020-01-20 16:31:24 +0100

    dot product

--- day00/ex00/sum.py
+++ day00/ex00/sum.py
@@ -28,8 +28,35 @@ def variance(x):
     return mean(lst)
 
 
-X1 = np.array([])
+def std(x):
+    try:
+        return variance(x)**0.5
+    except (TypeError) as e:
+        return None
+
+
+def dot(x, y):
+    mysum = 0.0
+    try:
+        if not isinstance(x, np.ndarray) or x.size == 0:
+            return None
+        if not isinstance(y, np.ndarray) or y.size == 0:
+            return None
+        if len(x) != len(y):
+            return None
+    except (TypeError) as e:
+        return None
+    else:
+        for i in range(len(x)):
+            mysum += float(x[i] * y[i])
+        return mysum
+
+
+Z = np.array([0])
 X = np.array([0, 15, -9, 7, 12, 3, -21])
+Y = np.array([2, 14, -13, 5, 12, 4, -19])
 # print(sum_(X, lambda x: x**2))
 # print(mean(X**2))
-print(variance(X/2))
+# print(variance(X))
+# print(std(Y))
+print(dot(Z, Y))
